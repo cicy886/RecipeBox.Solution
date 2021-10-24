61daa9bb8068be4b103bffad5a8b03f8639656df
Sisi Vieira <cicy886@gmail.com>
2021-10-24 14:20:16 -0700

    add rate and corresponding view
    Co-authored-by: Smita Raj<smita.raj12@gmail.com>

--- RecipeBox/Models/Recipe.cs
+++ RecipeBox/Models/Recipe.cs
@@ -11,6 +11,8 @@ namespace RecipeBox.Models
 
     public int RecipeId { get; set; }
     public string Description { get; set; }
+
+    public int Rate { get; set; }
     public virtual ApplicationUser User { get; set; }
 
     public virtual ICollection<CategoryRecipe> JoinEntities { get;}
